8d8df7f6215095055fd9f8795ddaedc4e77e6dcb
Kenny Song <kenny@ubuntu.(none)>
2013-08-22 01:34:58 -0700

    create page & form validation

--- main.js
+++ main.js
@@ -18,17 +18,62 @@ Template.browse.clubs = function () {
   return Clubs.find({}, {sort: {members: -1, clubname: 1}});
 }
 
-  // Template.hello.greeting = function () {
-  //   return "Welcome to clubs.";
-  // };
+Template.create.message = function () {
+  return Session.get('message');
+}
 
-  // Template.hello.events({
-  //   'click input' : function () {
-  //     // template data, if any, is available in 'this'
-  //     if (typeof console !== 'undefined')
-  //       console.log("You pressed the button");
-  //   }
-  // });
+  Template.create.events({
+    'click #create_submit' : function () {
+      event.preventDefault();
+
+      var f_name = $('#f_name').val();
+      var f_clubname = $('#f_clubname').val();
+      var f_netid = $('#f_netid').val();
+      var f_nationality = $('#f_nationality').val();
+      var f_description = $('#f_description').val();
+
+      var errors = [];
+
+      // alert(f_name + f_clubname + f_netid + f_nationality + f_description)
+
+      if (f_name == '') {
+        errors.push('Please enter a name.')
+      } else if (f_name.length > 100) {
+        errors.push('Name must be under 100 characters.')
+      }
+
+      if (f_clubname == '') {
+        errors.push('Please enter a club name.')
+      } else if (f_clubname.length > 100) {
+        errors.push('Club name must be under 100 characters.')
+      }      
+
+      if (f_netid == '') {
+        errors.push('Please enter a Net ID.')
+      } else if (f_netid.length > 100) {
+        errors.push('Net ID must be under 100 characters.')
+      }      
+
+      if (f_nationality == '') {
+        errors.push('Please enter a nationality.')
+      } else if (f_nationality.length > 100) {
+        errors.push('Nationality must be under 100 characters.')
+      }      
+
+      if (f_description == '') {
+        errors.push('Please enter a description.')
+      } else if (f_description.length > 100) {
+        errors.push('Description must be under 100 characters.')
+      }
+
+      if (errors.length != 0) {
+        Session.set('message', errors)
+      } else {
+        Session.set('message', ['Success!'])
+      }
+
+    }
+  });
 
 }
 
