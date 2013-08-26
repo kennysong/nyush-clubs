b55cf48e437f4857942bba8074c1fe1a0182653f
Kenny Song <kenny@ubuntu.(none)>
2013-08-26 07:58:37 -0700

    dynamic titles, removed "user pagge coming soon" tooltips

--- client/routing.js
+++ client/routing.js
@@ -7,32 +7,64 @@ Meteor.Router.add(
   {
     '/' : function() {
       Session.set("activeNav", Meteor.Router.page());
+      document.title = "NYU Shanghai Clubs";
       return 'browse'
     },
     '/create' : function() {
       Session.set("activeNav", Meteor.Router.page());
+      document.title = "Create a Club";
       return 'create'
     },
     '/how' : function() {
       Session.set("activeNav", Meteor.Router.page());
+      document.title = "How does this work?";
       return 'how'
     },
     '/new' : function() {
       Session.set("activeNav", 'browse');
+      document.title = "NYU Shanghai Clubs";
       return 'new'
     },
     '/club/:url' : function(url) {
         Session.set("activeNav", null);
         Session.set('club', url);
-        return 'club'; 
+
+        var club = Clubs.findOne({'url':url});
+
+        if (club) {
+          document.title = club.clubname;
+          return 'club'; 
+        } else {
+          return 'error'
+        }
+
     },
     '/add/:url' : function(url) {
         Session.set("activeNav", null);
       	Session.set('club', url);
-        return 'add'; 
+
+        var club = Clubs.findOne({'url':url});
+
+        if (club) {
+          document.title = 'Join ' + club.clubname;
+          return 'add'; 
+        } else {
+          return 'error'
+        }
     },
     '/user/:netid' : function(netid) {
-      return 'user'
+        var user = Users.findOne({'netid':netid});
+
+        if (user) {
+          document.title = user.name;
+          return 'user'; 
+        } else {
+          return 'error'
+        }
+
     },
-    '*' : 'error'
+    '*' : function() {
+      document.title = "Page not found!";
+      return 'error'
+    }
 });
